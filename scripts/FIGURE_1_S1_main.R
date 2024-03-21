4c719290cf94a35728e8f1f4ac8d32ba92a521fa
Atharva Bhagwat <41380551+atharva-bhagwat@users.noreply.github.com>
2024-03-21 16:29:04 -0400

    Update FIGURE_1_S1_main.R

--- scripts/FIGURE_1_S1_main.R
+++ scripts/FIGURE_1_S1_main.R
@@ -185,7 +185,7 @@ dev.off()
 
 
 # inferCNV
-infcnv = readRDS ('../../PM_scRNA_atlas/data/PM_infercnv.results.obj.Rds')
+infcnv = readRDS ('../../PM_scRNA_atlas/data/infercnv.results.obj.Rds')
 plot_cnv (infcnv)
 
 
